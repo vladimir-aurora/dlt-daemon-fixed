1a5e077c61151acb4bed4824aaa9475858a056a8
Vladimir Lozanovski <vladimir.lozanovski@auroralabs.com>
2024-02-08 16:19:57 +0100

    Fixed resource leak.

--- src/daemon/dlt-daemon.c
+++ src/daemon/dlt-daemon.c
@@ -576,20 +576,26 @@ int option_file_parser(DltDaemonLocal *daemon_local)
                     else if (strcmp(token, "RingbufferMinSize") == 0)
                     {
                         if (dlt_daemon_check_numeric_setting(token,
-                                value, &(daemon_local->RingbufferMinSize)) < 0)
+                                value, &(daemon_local->RingbufferMinSize)) < 0) {
+                            fclose (pFile);
                             return -1;
+                            }
                     }
                     else if (strcmp(token, "RingbufferMaxSize") == 0)
                     {
                         if (dlt_daemon_check_numeric_setting(token,
-                                value, &(daemon_local->RingbufferMaxSize)) < 0)
+                                value, &(daemon_local->RingbufferMinSize)) < 0) {
+                            fclose (pFile);
                             return -1;
+                            }
                     }
                     else if (strcmp(token, "RingbufferStepSize") == 0)
                     {
                         if (dlt_daemon_check_numeric_setting(token,
-                                value, &(daemon_local->RingbufferStepSize)) < 0)
+                                value, &(daemon_local->RingbufferMinSize)) < 0) {
+                            fclose (pFile);
                             return -1;
+                            }
                     }
                     else if (strcmp(token, "SharedMemorySize") == 0)
                     {
